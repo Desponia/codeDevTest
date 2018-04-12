df80a4c604637a9f0ae201c601d38053b95c43a2
desponia <coalcellar@gmail.com>
2018-04-12 18:47:30 +0900

    hacker rank 뭔소린지 알아먹지도 못해 ㅠ

---
+++ src/main/java/com/yuna/hacker/HackerRankDayOne.java
@@ -0,0 +1,65 @@
+package com.yuna.hacker;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.lang.reflect.Method;
+import java.math.BigInteger;
+import java.util.HashSet;
+
+import static java.lang.System.in;
+
+import java.util.Set;
+
+public class HackerRankDayOne {
+    private static final Logger logger = LoggerFactory.getLogger(HackerRankDayOne.class);
+
+    // https://www.hackerrank.com/challenges/prime-checker/problem
+    public static void main(String[] args) {
+        try {
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            int n1 = Integer.parseInt(br.readLine());
+            int n2 = Integer.parseInt(br.readLine());
+            int n3 = Integer.parseInt(br.readLine());
+            int n4 = Integer.parseInt(br.readLine());
+            int n5 = Integer.parseInt(br.readLine());
+            Prime ob = new Prime();
+            ob.checkPrime(n1);
+            ob.checkPrime(n1, n2);
+            ob.checkPrime(n1, n2, n3);
+            ob.checkPrime(n1, n2, n3, n4, n5);
+            Method[] methods = Prime.class.getDeclaredMethods();
+            Set<String> set = new HashSet<>();
+            boolean overload = false;
+            for (int i = 0; i < methods.length; i++) {
+                if (set.contains(methods[i].getName())) {
+                    overload = true;
+                    break;
+                }
+                set.add(methods[i].getName());
+
+            }
+            if (overload) {
+                throw new Exception("Overloading not allowed");
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+}
+
+class Prime {
+    void checkPrime(int... args) {
+        for (int num : args) {
+            // 소수 prime number
+            if(BigInteger.valueOf(num).isProbablePrime(1)) {
+                System.out.print(num + " ");
+            }
+        }
+        System.out.println();
+    }
+}
