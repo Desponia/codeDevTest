ccd9f57e9d32e73cbde3e739ca2fe4459fa48154
coalcellar <coalcellar@interpark.com>
2018-07-11 17:08:30 +0900

    java in action chap3

---
+++ src/main/java/com/yuna/inaction/Letter.java
@@ -0,0 +1,19 @@
+package com.yuna.inaction;
+
+/**
+ * author : jihye choi
+ */
+public class Letter {
+
+    public static String addHeader(String text) {
+        return String.format("From Raoul, Mario and Alan : %s", text);
+    }
+
+    public static String addFooter(String text) {
+        return String.format("%s Kind regards", text);
+    }
+
+    public static String checkSpelling(String text) {
+        return text.replaceAll("labda", "lambda");
+    }
+}
