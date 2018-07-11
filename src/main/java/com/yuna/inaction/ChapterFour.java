785413977bfbc86a8d6abf73c366e86ff58f0ede
coalcellar <coalcellar@interpark.com>
2018-07-11 21:15:48 +0900

    java in action chap4

---
+++ src/main/java/com/yuna/inaction/ChapterFour.java
@@ -0,0 +1,15 @@
+package com.yuna.inaction;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * author : jihye choi
+ */
+public class ChapterFour {
+
+    private static final Logger log = LoggerFactory.getLogger(ChapterFour.class);
+
+
+
+}
